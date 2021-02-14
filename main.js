874db1e1daa7fad547865a4c48bbefcc476e82b4
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-02-14 12:39:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,22 @@ recognition.onresult = function(event) {
     var msg = event.results[0][0].transcript;
     console.log(msg);
     document.getElementById("textbox").innerHTML = msg;
+    startSpeaking();
 }
+function startSpeaking() {
+    var synthesis = window.speechSynthesis;
+    var speak_data = document.getElementById("textbox").value;
+    var utterance = new SpeechSynthesisUtterance(speak_data);
+    synthesis.speak(utterance);
+    utterance.volume = 0.5;
+    utterance.rate = 2.5;
+    utterance.lang = "en-GB";
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: "png",
+    png_quality: 100
+});
+camera = document.getElementById("camera");
+Webcam.attach("camera");
