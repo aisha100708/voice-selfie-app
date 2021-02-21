a3fbb624eda6b999c05422fa881e2435f804aa3a
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-02-21 12:46:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,29 @@ recognition.onresult = function(event) {
     var msg = event.results[0][0].transcript;
     console.log(msg);
     document.getElementById("textbox").innerHTML = msg;
-    startSpeaking();
+    if (msg == "take my selfie") {
+        startSpeaking();
+    }
+    else {
+        var synthesis2 = window.speechSynthesis;
+        var speak_data2 = document.getElementById("textbox").value;
+        var utterance2 = new SpeechSynthesisUtterance(speak_data2);
+        synthesis2.speak(utterance2);
+    }
 }
 function startSpeaking() {
     var synthesis = window.speechSynthesis;
-    var speak_data = document.getElementById("textbox").value;
+    var speak_data = "taking your selfie in 5 seconds";
     var utterance = new SpeechSynthesisUtterance(speak_data);
     synthesis.speak(utterance);
     utterance.volume = 0.5;
-    utterance.rate = 2.5;
+    utterance.rate = 1;
     utterance.lang = "en-GB";
+    Webcam.attach("camera");
+    setTimeout(function(){
+        take_snapshot();
+        download_image();
+    }, 5000);
 }
 Webcam.set({
     width: 360,
@@ -27,4 +40,14 @@ Webcam.set({
     png_quality: 100
 });
 camera = document.getElementById("camera");
-Webcam.attach("camera");
+function take_snapshot() {
+    Webcam.snap(function(img_url){
+        document.getElementById("result").innerHTML = "<img id='selfie_output' src='"+img_url+"'>";
+    })
+}
+function download_image() {
+    link = document.getElementById("download_img");
+    new_img = document.getElementById("selfie_output").src;
+    link.href = new_img;
+    link.click();
+}
